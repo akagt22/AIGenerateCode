bed39a0055334603dd46ba432dd016fbb870c715
Okamoto Akihiro <ak.agt22@gmail.com>
2024-08-23 15:12:18 +0900

    Initial Commit

---
+++ AIGenerateCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIGenerateCode
+//
+//  Created by Okamoto Akihiro on 2024/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
